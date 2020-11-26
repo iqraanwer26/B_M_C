733ce75be84529bd111ad7c6d71e8c186f64165f
Iqraanwer26 <iqraanwer264@gmail.com>
2020-11-26 13:18:24 -0800

    container widget

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:bmi_pratic_for_london/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(BMICalculator());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
