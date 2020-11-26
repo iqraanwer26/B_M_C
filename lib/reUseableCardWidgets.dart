7f37a3ce072e787c33a33c60caf85b428890317d
Iqraanwer26 <iqraanwer264@gmail.com>
2020-11-26 13:43:56 -0800

    input page

--- lib/reUseableCardWidgets.dart
+++ lib/reUseableCardWidgets.dart
@@ -14,7 +14,7 @@ class ReuseableContainer extends StatelessWidget {
         margin: EdgeInsets.all(15.0),
         decoration: BoxDecoration(
           color: colorr,
-          borderRadius: BorderRadius.circular(10.0),
+          borderRadius: BorderRadius.circular(11.0),
         ),
       ),
     );
